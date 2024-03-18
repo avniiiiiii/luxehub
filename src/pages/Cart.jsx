4851dd5741c12d27be77aa5164968d8b0a743af3
avni <avniiiiiii07@gmail.com>
2024-03-18 15:29:52 +0530

    ya

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -17,7 +17,7 @@ const Cart = () => {
           <cartItem />
           <div className="w-1/3 bg-[#fafafa] py-6 px-4">
             <div className=" flex flex-col gap-6 border-b-[1px] border-b-gray-400 pb-6">
-              <h2 className="text-2xl font-medium ">$400</h2>
+              <h2 className="text-2xl font-medium ">Cart Total</h2>
               <p className="flex items-center gap-4 text-base">
                 {" "}
                 Subtotal{" "}
@@ -31,6 +31,15 @@ const Cart = () => {
                 </span>
               </p>
             </div>
+            <p className="font-titleFont font-semibold flex justify-between mt-6">
+              Total <span className="text-xl font-bold">${totalAmt}</span>
+            </p>
+            <button
+              onClick={handleCheckout}
+              className="text-base bg-black text-white w-full py-3 mt-6 hover:bg-gray-800 duration-300"
+            >
+              proceed to checkout
+            </button>
           </div>
         </div>
       </div>
