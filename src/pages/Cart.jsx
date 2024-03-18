41c3f916d7348646983eb0892b2a95a4b259b373
avni <avniiiiiii07@gmail.com>
2024-03-18 15:13:42 +0530

    yes

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -12,11 +12,12 @@ const Cart = () => {
           src="https://wallpapercave.com/wp/wp8678058.jpg"
           alt="cartImg"
         />
-        <div>
-          <CartItem />{" "}
+        <div className="max-w-screen-xl mx-auto py-20 flex">
+          <CartItem />
           <div className="w-1/3 bg-[#fafafa] py-6 px-4">
-            {" "}
-            <div className=" flex flex-col gap-6 border-b-[1px] border-b-gray-400 pb-6"></div>
+            <div className=" flex flex-col gap-6 border-b-[1px] border-b-gray-400 pb-6">
+              <h2 className="text-2xl font-medium ">Cart Total</h2>
+            </div>
           </div>
         </div>
       </div>
