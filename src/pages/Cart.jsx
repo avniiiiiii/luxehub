cbb0af54a3460e1fcf60db38042760a01cb5782d
avni <avniiiiiii07@gmail.com>
2024-03-18 15:34:55 +0530

    yespush

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import cartItem from "../components/cartItem.jsx";
+import Cartitem from "../components/cartItem.jsx";
 const Cart = () => {
   const productData = useSelector((state) => state.luxehub.productData);
   console.log(productData);
@@ -14,7 +14,7 @@ const Cart = () => {
           alt="cartImg"
         />
         <div className="max-w-screen-xl mx-auto py-20 flex">
-          <cartItem />
+          <Cartitem />
           <div className="w-1/3 bg-[#fafafa] py-6 px-4">
             <div className=" flex flex-col gap-6 border-b-[1px] border-b-gray-400 pb-6">
               <h2 className="text-2xl font-medium ">Cart Total</h2>
@@ -32,12 +32,9 @@ const Cart = () => {
               </p>
             </div>
             <p className="font-titleFont font-semibold flex justify-between mt-6">
-              Total <span className="text-xl font-bold">${totalAmt}</span>
+              Total <span className="text-xl font-bold">$889</span>
             </p>
-            <button
-              onClick={handleCheckout}
-              className="text-base bg-black text-white w-full py-3 mt-6 hover:bg-gray-800 duration-300"
-            >
+            <button className="text-base bg-black text-white w-full py-3 mt-6 hover:bg-gray-800 duration-300">
               proceed to checkout
             </button>
           </div>
