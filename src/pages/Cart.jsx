319e8b31507ea823506289b4c919359f318b9637
avni <avniiiiiii07@gmail.com>
2024-03-18 15:22:35 +0530

    hn

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -18,6 +18,20 @@ const Cart = () => {
           <div className="w-1/3 bg-[#fafafa] py-6 px-4">
             <div className=" flex flex-col gap-6 border-b-[1px] border-b-gray-400 pb-6">
               <h2 className="text-2xl font-medium ">Cart Total</h2>
+              <p className="flex items-center gap-4 text-base">
+                {" "}
+                Subtotal{" "}
+                <span className="font-titleFont font-bold text-lg">
+                  ${totalAmt}
+                </span>
+              </p>
+              <p className="flex items-start gap-4 text-base">
+                Shipping{" "}
+                <span>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Quos, veritatis.
+                </span>
+              </p>
             </div>
           </div>
         </div>
