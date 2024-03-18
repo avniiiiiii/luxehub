9be7f1a85146df7c2c7e47e0ef0916944b431b73
avni <avniiiiiii07@gmail.com>
2024-03-18 15:19:48 +0530

    yes

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
+import cartItem from "../components/cartItem.jsx";
 const Cart = () => {
   const productData = useSelector((state) => state.luxehub.productData);
+  console.log(productData);
 
   return (
     <>
@@ -13,7 +14,7 @@ const Cart = () => {
           alt="cartImg"
         />
         <div className="max-w-screen-xl mx-auto py-20 flex">
-          <CartItem />
+          <cartItem />
           <div className="w-1/3 bg-[#fafafa] py-6 px-4">
             <div className=" flex flex-col gap-6 border-b-[1px] border-b-gray-400 pb-6">
               <h2 className="text-2xl font-medium ">Cart Total</h2>
