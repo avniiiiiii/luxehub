6d58780db4e4c2623c2a9535668f30d68f58b4c4
avni <avniiiiiii07@gmail.com>
2024-03-22 12:33:38 +0530

    yes

--- src/components/cartItem.jsx
+++ src/components/cartItem.jsx
@@ -16,7 +16,7 @@ const Cartitem = () => {
             className="flex items-center justify-between gap-6 mt-6"
           >
             <div className="flex items-center gap-2">
-              <MdOutlineClose />
+              <MdOutlineClose className="text-xl text-gray-600 hover:text-red-600 cursor-pointer duration-300" />
             </div>
           </div>;
         })}
