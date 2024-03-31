a6a519960b4e080069bd1ec7785ad55ae4fed901
avni <avniiiiiii07@gmail.com>
2024-03-31 23:51:48 +0530

    o

--- src/reduxSlice/Store.jsx
+++ src/reduxSlice/Store.jsx
@@ -1,4 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import luxehubReducer from "./luxehubSlice.jsx";
 
 export const store = configureStore({
