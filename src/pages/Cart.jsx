ef2874fd213f5d96baabbb4d2d2f177e7c4f1f2f
avni <avniiiiiii07@gmail.com>
2024-03-31 17:28:55 +0530

    y

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -39,6 +39,18 @@ const Cart = () => {
             </button>
           </div>
         </div>
+        <ToastContainer
+          position="top-left"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
       </div>
     </>
   );
