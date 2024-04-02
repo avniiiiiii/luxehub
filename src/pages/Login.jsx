a737be43147578d00cb329e3be299c9bd665a6a7
avni <avniiiiiii07@gmail.com>
2024-04-02 15:18:17 +0530

    yes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,9 +3,10 @@ import clientGoogle from "../assets/clientGoogle.jpg";
 
 const Login = () => {
   return (
-    <div>
-      <div>
-        <img src={clientGoogle} alt="logogoogle" />
+    <div className="w-full flex flex-col items-center justify-center gap-10 py-20">
+      <div className="w-full flex items-center justify-center gap-10">
+        <img src={clientGoogle} alt="logogoogle" />{" "}
+        <span className="text-sm text-gray-900"> Sign in with Google</span>
       </div>
     </div>
   );
