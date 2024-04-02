99094ce96a5397e224634cbbd5f76f685a717aa7
avni <avniiiiiii07@gmail.com>
2024-04-02 15:19:48 +0530

    u

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,8 +5,10 @@ const Login = () => {
   return (
     <div className="w-full flex flex-col items-center justify-center gap-10 py-20">
       <div className="w-full flex items-center justify-center gap-10">
-        <img src={clientGoogle} alt="logogoogle" />{" "}
-        <span className="text-sm text-gray-900"> Sign in with Google</span>
+        <div className="text-base w-60 h-12 tracking-wide border-[1px] border-gray-400 rounded-md flex items-center justify-center gap-2 hover:border-blue-600 cursor-pointer duration-300">
+          <img src={clientGoogle} alt="logogoogle" />
+          <span className="text-sm text-gray-900"> Sign in with Google</span>
+        </div>
       </div>
     </div>
   );
