a1c959934177f0ddc55b5ef38b1ac4d80d923980
avni <avniiiiiii07@gmail.com>
2024-04-03 15:56:27 +0530

    yes

--- src/Firebase.config.jsx
+++ src/Firebase.config.jsx
@@ -1,14 +1,16 @@
-/// Import the functions you need from the Firebase SDK
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  projectId: "your-project-id",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "your-app-id",
+  apiKey: "AIzaSyCyEsvI0D66wyFBVB6CAYPp_glA3OSzqk8",
+  authDomain: "bazar-d3cb2.firebaseapp.com",
+  projectId: " luxehub-app",
+  storageBucket: "bazar-d3cb2.appspot.com",
+  messagingSenderId: "290833778260",
+  appId: "1:290833778260:web:56aa636d4d6a3a38e48fc1",
 };
 
 // Initialize Firebase
